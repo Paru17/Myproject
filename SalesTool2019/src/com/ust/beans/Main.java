8bce11a561f595d74f44ea93186161dc744927dc
dev5 <dev5@dev5-G41T-M7>
2019-09-24 15:11:55 +0530

    Second commit

--- SalesTool2019/src/com/ust/beans/Main.java
+++ SalesTool2019/src/com/ust/beans/Main.java
@@ -3,7 +3,7 @@ package com.ust.beans;
 public class Main {
 	public static void main(String [] args) {
 		SalesData objSalesData = new SalesData();
-		System.out.println("Welcome to Sales App");
+		System.out.println("Welcome to Sales App v2019");
 		
 	}
 
